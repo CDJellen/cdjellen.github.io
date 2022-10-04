8a74ad50e76944c74d1ec85e8695e409d1702da7
CDJellen <64814231+CDJellen@users.noreply.github.com>
2022-10-03 17:32:09 -0700

    updates

--- gh-pages.js
+++ gh-pages.js
@@ -3,7 +3,7 @@ import { publish } from "gh-pages";
 publish(
     "web_dev",
     {
-        branch: "gh-pages",
+        branch: "docs",
         repo: "https://github.com/cdjellen/cdjellen.github.io.git",
         user: {
         name: "Chris Jellen",
