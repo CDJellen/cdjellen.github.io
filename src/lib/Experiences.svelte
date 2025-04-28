cb5d1e97ded803c9dbe1e17e3f6543fdfa358fc3
cdjellen <cdjellen@gmail.com>
2025-04-27 20:25:51 -0700

    update resume

--- src/lib/Experiences.svelte
+++ src/lib/Experiences.svelte
@@ -15,10 +15,10 @@
       link="https://www.wraithwatch.com/"
       name="Wraithwatch"
       description="Security Engineering | Machine Learning Engineering"
-      descriptionLong="Wraithwatch is a cybersecurity startup building the next generation of AI/ML-informed cyber defense. As an early member of the team, I've had the opportunity to build the product, own customer relationships, and help drive strategic direction."
+      descriptionLong="Wraithwatch is a cybersecurity startup building the next generation of AI/ML-informed cyber defense. As an early member of the team, I've had the opportunity to build the product, own infrastructure management, and develop customer relationships."
       notes={[
-        "Lead the design and development of security automation workflows.",
-        "Managing the product roadmap through synthesis of partner feedback, engineering resources, and machine learning research.",
+        "Lead the design, development, and implementation of resilient infrastructure, decreasing product downtime by over 8 times.",
+        "Developed core backend services, allowing the product to scale from hundreds to millions of entities under analysis.",
       ]}
     />
     <Experience
@@ -38,8 +38,8 @@
       image={MitreLogo}
       link="https://www.mitre.org"
       name="The MITRE Corporation"
-      description="Software Engineering | Machine Learning Engineering"
-      descriptionLong="In my time with MITRE, I've supported The Veteran's Benefits Administration, the United States Marine Corps, and Intelligence Community as an engineer and leader. I've learned first-hand the importance of building a strong understanding of customer needs, clear communication, and strategic planning. Leading the development of software prototypes allowed me to craft a product through the full development lifecycle, from roadmapping to ticketing to delivery and every step in between."
+      description="Machine Learning Engineering | Data Science"
+      descriptionLong="Supported The Veteran's Benefits Administration, the United States Marine Corps, and Intelligence Community as an engineer and consultant, delivering deep technical analyses in support of intelligence automation."
       notes={[
         "Developed prototype AI/ML-informed quality assurance tools for Public-sector Data Scientists which scale to hundreds of millions of transactions.",
         "Doubled the frequency of cross-organization engagements through agile project management and customer-centric design.",
@@ -50,11 +50,11 @@
       image={CalderaLogo}
       link="https://caldera.mitre.org"
       name="CALDERA: Automated Adversary Emulation"
-      description="Product Lead | Engineering Manager"
+      description="Engineering Manager"
       descriptionLong="CALDERA is an open-source framework for automated adversary emulation. It offers a powerful combination of atomic abilities, automated attack planning, and stealth-focused C2 capabilities to make cyber risk analysis accessible. CALDERA is used both by the open source community and a range of Government, DoD, IC, and university sponsors. Building experience as a leader helped me to understand the importance of effective management in successful product development. In planning the future of CALDERA, I learned effective product management in the context of competing customer interests and priorities."
       notes={[
-        "Product lead for CALDERA's cyber ontological mapping capability, interfaced with a range of DoD sponsors to ensure wide interoperability and wider use of CALDERA as a cyber analytics tool.",
-        "Led a team of four (3 engineers, 1 data scientist) to develop novel offensive cyber planning capabilities and data management solutions.",
+        "Product lead for CALDERA's cyber ontological mapping capability, interfaced with a range of DoD sponsors to ensure wide interoperability and wider use of CALDERA as a cyber analytic tool.",
+        "Led a team of four (3 engineers, 1 data scientist) to develop novel offensive cyber planning capabilities and data management solutions, resulting in a software patent and new opportunities for Government partnership.",
         "Built closed-source AI-ML enabled cyber posture analysis capabilities deployed on AWS.",
       ]}
     />
